f0fd6766bde4654205f0f71990c6da74c06d63e5
Dimitris Zorbas <zorbash@skroutz.gr>
2014-12-11 22:22:19 +0200

    Add cmake to workspace puppet

--- puppet/workstation/init.pp
+++ puppet/workstation/init.pp
@@ -10,6 +10,7 @@ class essential_packages {
               'pbuilder',
               'mc', # provides midnight commander
               'lm-sensors', # provides sensors (for hw temperature monitoring)
+              'cmake',
               'tor']:
     ensure => installed,
   }
